37f11a4e93c9d5a9369ebf91716c070415a4ea15
Marybeth Snodgrass <msnodgrass25@gmail.com>
2016-03-07 15:52:01 -0600

    complete list

---
+++ stack.js
@@ -0,0 +1,14 @@
+'use strict'
+
+class Node {
+    constructor (data) {
+        this.payload = data
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor () {
+        this.head = null
+    }
+}
