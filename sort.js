e41bea3845996ff53bba3cfe0a9c229c3e4b17c8
Marybeth Snodgrass <msnodgrass25@gmail.com>
2016-03-09 11:45:11 -0600

    added package.json file, installed node modules lodash and chalk, added dictionary.js and sort.js files

---
+++ sort.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const chalk = require('chalk')
+const util = require('util')
+
+const arr = [6,5,3,1,8,7,2,4]
+
+console.log('start', arr)
+
+function swap (array, index1, index2) {
+  const temp = array[index1]
+
+  array[index1] = array[index2]
+  array[index2] = temp
+}
+
+function bubbleSort (array) {
+  for (let j = 1; j < array.length; j++) {
+    for (let i = 0; i < array.length - j; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1)
+        console.log(chalk.red(util.inspect(array)))
+      }
+      console.log(chalk.dim(util.inspect(array)))
+    }
+  }
+
+  return array
+}
+
+console.log('bubbled', bubbleSort(arr))
